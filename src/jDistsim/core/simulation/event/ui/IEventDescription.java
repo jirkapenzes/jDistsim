c03822e465627aafc3047cc2d27b28f6707f1272
jirkapenzes <jirkapenzes@hotmail.com>
2012-10-06 21:27:19 +0200

    Interface for event description.

---
+++ src/jDistsim/core/simulation/event/ui/IEventDescription.java
@@ -0,0 +1,14 @@
+package jDistsim.core.simulation.event.ui;
+
+/**
+ * Author: Jirka Pénzeš
+ * Date: 29.9.12
+ * Time: 17:49
+ */
+public interface IEventDescription {
+
+    public String getTitle();
+
+    public String getDescription();
+
+}
