e42e936c447852337f210461e5504f96363e48e0
jirkapenzes <jirkapenzes@hotmail.com>
2012-10-07 00:24:55 +0200

    Added dispose text.

--- src/jDistsim/utils/resource/EventTextResource.java
+++ src/jDistsim/utils/resource/EventTextResource.java
@@ -8,7 +8,9 @@ package jDistsim.utils.resource;
 public class EventTextResource {
 
     public static String EMPTY_EVENT_DESCRIPTION_TITLE = "Information about process";
-    public static String EMPTY_EVENT_DESCRIPTION_TEXT = "Click on the component in the toolbox, and there appears a short information.";
+    public static String EMPTY_EVENT_DESCRIPTION_TEXT = "<html>Click on the component in the toolbox, and there appears a short information.</html>";
     public static String CREATE_EVENT_DESCRIPTION_TITLE = "Create";
     public static String CREATE_EVENT_DESCRIPTION_TEXT = "<html>Hold the entity in the DELAY block for a time sampled from a Uniform distribution between 3 and 5 time units.</html>";
+    public static String DISPOSE_EVENT_DESCRIPTION_TITLE = "Dispose";
+    public static String DISPOSE_EVENT_DESCRIPTION_TEXT = "<html>Some text description ...</html>";
 }
