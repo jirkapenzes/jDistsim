de7f133953376ed2c92114ea14807d67336ef468
jirkapenzes <jirkapenzes@hotmail.com>
2012-10-07 00:24:34 +0200

    Rename and fix toolbar event preview.

--- src/jDistsim/designer/ui/control/event/ToolbarEventPreviewControl.java
+++ src/jDistsim/designer/ui/control/event/ToolbarEventPreviewControl.java
@@ -1,5 +1,6 @@
 package jDistsim.designer.ui.control.event;
 
+import jDistsim.core.simulation.event.ui.UIEventPreview;
 import jDistsim.designer.ui.UIConfiguration;
 
 import javax.swing.*;
@@ -16,13 +17,13 @@ import java.awt.image.BufferedImage;
  * Date: 29.9.12
  * Time: 19:41
  */
-public class ToolbarViewEventControl extends JComponent {
+public class ToolbarEventPreviewControl extends JComponent {
 
-    private EventControl eventControl;
+    private UIEventPreview eventPreview;
     private boolean active;
 
-    public ToolbarViewEventControl(EventControl eventControl) {
-        this.eventControl = eventControl;
+    public ToolbarEventPreviewControl(UIEventPreview eventPreview) {
+        this.eventPreview = eventPreview;
         initializeComponents();
     }
 
@@ -33,8 +34,14 @@ public class ToolbarViewEventControl extends JComponent {
 
         addMouseListener(new MouseAdapter() {
             @Override
-            public void mouseClicked(MouseEvent e) {
+            public void mouseEntered(MouseEvent mouseEvent) {
                 setActive(true);
+                repaint();
+            }
+
+            @Override
+            public void mouseExited(MouseEvent mouseEvent) {
+                setActive(false);
                 repaint();
             }
         });
@@ -54,22 +61,24 @@ public class ToolbarViewEventControl extends JComponent {
     }
 
     @Override
-    protected void paintComponent(Graphics g) {
-        super.paintComponent(g);
-        Graphics2D graphics2D = (Graphics2D) g;
+    protected void paintComponent(Graphics graphics) {
+        super.paintComponent(graphics);
+        Graphics2D graphics2D = (Graphics2D) graphics;
         graphics2D.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
 
         if (isActive()) {
-            graphics2D.setColor(new Color(217, 234, 242));
+            graphics2D.setColor(new Color(230, 240, 251));
             graphics2D.fillRect(0, 0, getWidth(), getHeight());
+            graphics2D.setColor(new Color(201, 224, 248));
+            graphics2D.drawRect(0, 0, getWidth() - 1, getHeight() - 1);
         }
 
         BufferedImage bufferedImage = new BufferedImage(70, 50, BufferedImage.TYPE_INT_ARGB);
-        eventControl.draw(bufferedImage.createGraphics(), 70, 50);
-        g.drawImage(bufferedImage, 0, 0, this);
+        eventPreview.display(bufferedImage.createGraphics(), 70, 50);
+        graphics.drawImage(bufferedImage, 0, 0, this);
 
         Font font = UIConfiguration.getInstance().getDefaultFont();
-        String title = eventControl.getEventDescription().getTitle();
+        String title = eventPreview.getEventDescription().getTitle();
         FontRenderContext context = graphics2D.getFontRenderContext();
         TextLayout txt = new TextLayout(title, font, context);
         Rectangle2D bounds = txt.getBounds();
