c5bff4e08d0e56687a7b07008cae18df0dc5362c
jirkapenzes <jirkapenzes@hotmail.com>
2012-10-06 21:25:56 +0200

    Added text resources for event descriptions.

---
+++ src/jDistsim/utils/resource/EventTextResource.java
@@ -0,0 +1,14 @@
+package jDistsim.utils.resource;
+
+/**
+ * Author: Jirka Pénzeš
+ * Date: 6.10.12
+ * Time: 20:31
+ */
+public class EventTextResource {
+
+    public static String EMPTY_EVENT_DESCRIPTION_TITLE = "Information about process";
+    public static String EMPTY_EVENT_DESCRIPTION_TEXT = "Click on the component in the toolbox, and there appears a short information.";
+    public static String CREATE_EVENT_DESCRIPTION_TITLE = "Create";
+    public static String CREATE_EVENT_DESCRIPTION_TEXT = "<html>Hold the entity in the DELAY block for a time sampled from a Uniform distribution between 3 and 5 time units.</html>";
+}
