ab1b1cc68c354253d5d8bde9a6ce3a5353ac7c0a
jirkapenzes <jirkapenzes@hotmail.com>
2012-10-06 21:29:46 +0200

    Remove space.

--- src/jDistsim/utils/resource/TextResources.java
+++ src/jDistsim/utils/resource/TextResources.java
@@ -10,7 +10,6 @@ public class TextResources {
     public static String APPLICATION_NAME = "jDistsim:designer beta - model1.jdsim";
     public static String INFORMATION_PANEL_TITLE = "Project bar";
     public static String TOOLBAR_EVENT_PANEL_TITLE = "Event toolbar";
-
     public static String NULL_TEXT = "Empty string";
 
 }
