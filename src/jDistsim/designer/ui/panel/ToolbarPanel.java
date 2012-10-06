9b251a6d3bb6a2e442fba357e1d9b6b5e919dbe5
jirkapenzes <jirkapenzes@hotmail.com>
2012-10-06 19:28:48 +0200

    Added new icons and fix background color.

--- src/jDistsim/designer/ui/panel/ToolbarPanel.java
+++ src/jDistsim/designer/ui/panel/ToolbarPanel.java
@@ -24,7 +24,9 @@ public class ToolbarPanel extends JPanel {
         setLayout(new FlowLayout(FlowLayout.LEFT, 2, 2));
 
         setBorder(new EmptyBorder(0, 0, 0, 0));
-        add(new ImageButton(Resources.getImage("system/menubar-icon-start.png"), new IconBackgroundColorHoverStyle(Color.blue), new Dimension(16, 16), 5));
+        add(new ImageButton(Resources.getImage("system/toolbar-icon-new.png"), new IconBackgroundColorHoverStyle(), new Dimension(16, 16), 5));
+        add(new ImageButton(Resources.getImage("system/toolbar-icon-open.png"), new IconBackgroundColorHoverStyle(), new Dimension(16, 16), 5));
+        add(new ImageButton(Resources.getImage("system/toolbar-icon-save-as.png"), new IconBackgroundColorHoverStyle(), new Dimension(16, 16), 5));
         add(new MenuSeparator());
     }
 
