bec95d6b418afa8fb9bd41d0aa57650157b6d773
jirkapenzes <jirkapenzes@hotmail.com>
2012-10-28 01:52:27 +0200

    Use memory watcher in status bar.

--- src/jDistsim/application/designer/controller/StatusBarController.java
+++ src/jDistsim/application/designer/controller/StatusBarController.java
@@ -2,6 +2,9 @@ package jDistsim.application.designer.controller;
 
 import jDistsim.application.designer.model.StatusBarModel;
 import jDistsim.application.designer.view.StatusBarView;
+import jDistsim.utils.event.ActionArgument;
+import jDistsim.utils.event.ActionObjectListener;
+import jDistsim.utils.gc.MemoryWatcher;
 import jDistsim.utils.logging.LogMessage;
 import jDistsim.utils.logging.Logger;
 import jDistsim.utils.logging.handlers.ILoggerHandler;
@@ -17,10 +20,21 @@ import jDistsim.utils.pattern.observer.IObserver;
  */
 public class StatusBarController extends AbstractController<StatusBarModel> implements ILoggerHandler, IObserver {
 
+    private final MemoryWatcher memoryWatcher;
+
     public StatusBarController(AbstractFrame mainFrame, StatusBarModel statusBarModel) {
         super(mainFrame, statusBarModel);
         Logger.getLoggerHandlerManager().addHandler(this);
         getModel().addObserver(this);
+
+        memoryWatcher = new MemoryWatcher();
+        memoryWatcher.addActionObjectListener(new ActionObjectListener() {
+            @Override
+            public void ActionPerformed(ActionArgument actionArgument) {
+                getModel().setRightContentText("Current used memory " + memoryWatcher.getUsedMemory() + "M of " + memoryWatcher.getTotalMemory() + "M");
+            }
+        });
+        new Thread(memoryWatcher).start();
     }
 
     @Override
