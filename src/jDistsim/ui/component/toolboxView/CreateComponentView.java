b45f8cfb4043d17271be56344a44f3ed553d0121
jirkapenzes <jirkapenzes@hotmail.com>
2012-11-09 21:14:39 +0100

    Fix "Create" component view.

--- src/jDistsim/ui/component/toolboxView/CreateComponentView.java
+++ src/jDistsim/ui/component/toolboxView/CreateComponentView.java
@@ -1,6 +1,7 @@
 package jDistsim.ui.component.toolboxView;
 
 import jDistsim.ui.component.ComponentView;
+import jDistsim.utils.math.PointUtilities;
 
 import javax.swing.*;
 import java.awt.*;
@@ -12,19 +13,38 @@ import java.awt.*;
  */
 public class CreateComponentView extends ComponentView {
 
-    public CreateComponentView() {
-    }
-
     @Override
     protected JComponent makeView() {
-        return new JPanel() {
-            @Override
-            protected void paintComponent(Graphics g) {
-                super.paintComponent(g);
-                System.out.println(getWidth() + "x" + getHeight());
-                g.setColor(Color.red);
-                g.fillRect(0, 0, getWidth(), getHeight());
-            }
-        };
+        return new CreateComponent();
+    }
+
+    private class CreateComponent extends JComponent {
+
+        public CreateComponent() {
+            setOpaque(false);
+        }
+
+        @Override
+        protected void paintComponent(Graphics graphics) {
+            super.paintComponent(graphics);
+
+            Graphics2D graphics2D = (Graphics2D) graphics;
+            graphics2D.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+
+            Polygon polygon = new Polygon();
+            polygon.addPoint(1,1);
+            polygon.addPoint(PointUtilities.byPercentageOnLine(60, getWidth()), 1);
+            polygon.addPoint(getWidth() - 1, getHeight() / 2-1);
+            polygon.addPoint(PointUtilities.byPercentageOnLine(60, getWidth()), getHeight()-1);
+            polygon.addPoint(1, getHeight()-1);
+
+            graphics2D.setColor(new Color(67,201,224));
+            graphics2D.fillPolygon(polygon);
+
+            BasicStroke lineStroke = new BasicStroke(2, BasicStroke.CAP_ROUND, BasicStroke.JOIN_ROUND);
+            graphics2D.setColor(new Color(70,127,137));
+            graphics2D.setStroke(lineStroke);
+            graphics2D.drawPolygon(polygon);
+        }
     }
 }
