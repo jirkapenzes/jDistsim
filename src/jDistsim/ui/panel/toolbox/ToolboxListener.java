8fe0ca0867dbe92747c9b7f398eec7369d538c95
jirkapenzes <jirkapenzes@hotmail.com>
2012-11-09 22:51:47 +0100

    Added listener.

---
+++ src/jDistsim/ui/panel/toolbox/ToolboxListener.java
@@ -0,0 +1,14 @@
+package jDistsim.ui.panel.toolbox;
+
+import jDistsim.application.designer.model.ToolboxModelItem;
+
+/**
+ * Author: Jirka Pénzeš
+ * Date: 9.11.12
+ * Time: 21:21
+ */
+public interface ToolboxListener {
+
+    public void componentSelected(ToolboxModelItem modelItem);
+
+}
