f198a68995f7911cce30c7d363a7874e4a6390a4
jirkapenzes <jirkapenzes@hotmail.com>
2012-11-09 21:11:16 +0100

    Use toolbox model.

--- src/jDistsim/ui/panel/toolbox/ToolboxPanel.java
+++ src/jDistsim/ui/panel/toolbox/ToolboxPanel.java
@@ -1,9 +1,7 @@
 package jDistsim.ui.panel.toolbox;
 
-import jDistsim.core.model.ToolboxModel;
-import jDistsim.core.module.IEventToolbarModule;
-import jDistsim.ui.component.ComponentView;
-import jDistsim.ui.component.toolboxView.CreateComponentView;
+import jDistsim.application.designer.model.ToolboxModel;
+import jDistsim.application.designer.model.ToolboxModelItem;
 import jDistsim.ui.panel.InternalPanel;
 import jDistsim.utils.logging.Logger;
 import jDistsim.utils.resource.TextResources;
@@ -12,6 +10,8 @@ import jDistsim.utils.ui.WrapLayout;
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
 import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 /**
  * Author: Jirka Pénzeš
@@ -20,12 +20,12 @@ import java.awt.*;
  */
 public class ToolboxPanel extends InternalPanel {
 
+    private JPanel controls;
     private ToolboxModel toolboxModel;
 
-    public ToolboxPanel(ToolboxModel toolboxModel) {
+    public ToolboxPanel() {
         super(TextResources.TOOLBAR_EVENT_PANEL_TITLE);
         Logger.log("Initialize event toolbox panel");
-        this.toolboxModel = toolboxModel;
         initializeComponents();
     }
 
@@ -35,20 +35,32 @@ public class ToolboxPanel extends InternalPanel {
         scrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
         scrollPane.setBorder(new EmptyBorder(0, 0, 0, 0));
 
-        JPanel controls = new JPanel();
+        controls = new JPanel();
         controls.setLayout(new WrapLayout(FlowLayout.LEFT, 10, 0));
         controls.setBackground(Color.white);
-        controls.setBorder(new EmptyBorder(10, 0, 0, 0));
+        controls.setBorder(new EmptyBorder(2, 0, 0, 0));
 
         scrollPane.setViewportView(controls);
         add(scrollPane, BorderLayout.CENTER);
         add(new ToolboxDescriptionPanel(), BorderLayout.SOUTH);
+    }
 
-        for (IEventToolbarModule toolbarModule : toolboxModel.getModules()) {
-            // controls.add(new ToolbarEventPreviewControl(toolbarModule.getEventPreview()));
+    public void setModel(ToolboxModel toolboxModel) {
+        this.toolboxModel = toolboxModel;
+
+        controls.removeAll();
+        for (ToolboxModelItem item : toolboxModel.getItems()) {
+            ToolboxButton toolboxButton = new ToolboxButton(item.getComponentView(), "Create");
+            toolboxButton.setMouseEnteredMode(true);
+            toolboxButton.addMouseListener(new MouseAdapter() {
+                @Override
+                public void mouseClicked(MouseEvent event) {
+                    ToolboxButton source = (ToolboxButton) event.getSource();
+                    source.setActive(!source.isActive());
+                }
+            });
+            toolboxButton.setPreferredSize(new Dimension(75, 65));
+            controls.add(toolboxButton);
         }
-
-        ComponentView componentView = new CreateComponentView();
-        controls.add(new ToolboxButton(componentView));
     }
 }
