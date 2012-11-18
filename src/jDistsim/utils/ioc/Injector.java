6ee58f1cc80f4dffb4088b98324585d275c5f2ee
Jirka Pénzeš <jirkapenzes@hotmail.com>
2012-11-18 14:20:49 +0100

    Added Injector (only class skeleton)

---
+++ src/jDistsim/utils/ioc/Injector.java
@@ -0,0 +1,17 @@
+package jDistsim.utils.ioc;
+
+/**
+ * Author: Jirka Pénzeš
+ * Date: 16.11.12
+ * Time: 12:11
+ */
+public class Injector {
+
+    public <T> void bind(Class<T> type) {
+
+    }
+
+    public <T> T inject(Class<T> type) {
+        return null;
+    }
+}
