38c0219caf28fbf0495559d877a89185b560b448
Jirka Pénzeš <jirkapenzes@hotmail.com>
2012-11-26 12:52:26 +0100

    Added model space listener

---
+++ src/jDistsim/ui/panel/workspace/ModelSpaceListener.java
@@ -0,0 +1,13 @@
+package jDistsim.ui.panel.workspace;
+
+import jDistsim.core.modules.Module;
+
+/**
+ * Author: Jirka Pénzeš
+ * Date: 24.11.12
+ * Time: 21:27
+ */
+public interface ModelSpaceListener {
+
+    void selectedModule(Module module);
+}
