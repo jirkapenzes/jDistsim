a077a242901798d6f4153ff736170f85f062b972
Jirka Pénzeš <jirkapenzes@hotmail.com>
2013-02-10 13:57:19 +0100

    Added observable collection

---
+++ src/jDistsim/utils/ObservableHashMap.java
@@ -0,0 +1,37 @@
+package jDistsim.utils;
+
+import jDistsim.utils.pattern.observer.Observable;
+
+import java.util.Collection;
+import java.util.HashMap;
+
+/**
+ * Author: Jirka Pénzeš
+ * Date: 10.2.13
+ * Time: 13:37
+ */
+public class ObservableHashMap<Key, Value> extends Observable {
+
+    private HashMap<Key, Value> hashMap;
+
+    public ObservableHashMap() {
+        hashMap = new HashMap<>();
+    }
+
+    public void put(Key key, Value value) {
+        hashMap.put(key, value);
+        notifyObservers(value);
+    }
+
+    public Value get(Key key) {
+        return hashMap.get(key);
+    }
+
+    public Value remove(Key key) {
+        return hashMap.remove(key);
+    }
+
+    public Collection<Value> values() {
+        return hashMap.values();
+    }
+}
