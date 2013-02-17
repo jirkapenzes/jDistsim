01453406e1ad65aa8bf8f3e873d500a61fcd5b28
Jirka Pénzeš <jirkapenzes@hotmail.com>
2013-02-17 12:24:32 +0100

    Added method logging

--- src/jDistsim/application/designer/controller/modelSpaceFeature/SelectedActiveModuleAction.java
+++ src/jDistsim/application/designer/controller/modelSpaceFeature/SelectedActiveModuleAction.java
@@ -3,6 +3,7 @@ package jDistsim.application.designer.controller.modelSpaceFeature;
 import jDistsim.application.designer.controller.ModelSpaceController;
 import jDistsim.core.modules.ModuleUI;
 import jDistsim.utils.common.ModelSpaceListener;
+import jDistsim.utils.logging.Logger;
 
 import java.awt.event.MouseEvent;
 
@@ -15,6 +16,7 @@ public class SelectedActiveModuleAction extends ModelSpaceListener {
 
     @Override
     public void modelSpaceMouseClicked(MouseEvent mouseEvent, ModelSpaceController modelSpaceController) {
+        Logger.log("unselected active module");
         modelSpaceController.unselectedActiveModule();
     }
 
@@ -22,12 +24,13 @@ public class SelectedActiveModuleAction extends ModelSpaceListener {
     public void moduleClicked(MouseEvent mouseEvent, ModelSpaceController modelSpaceController) {
         ModuleUI moduleUI = (ModuleUI) mouseEvent.getSource();
         if (moduleUI == modelSpaceController.getModel().getCurrentActiveModule() && moduleUI.getActive()) {
+            Logger.log("unselected active module");
             modelSpaceController.unselectedActiveModule();
             return;
         }
 
+        Logger.log("selected active module");
         modelSpaceController.unselectedActiveModule();
         modelSpaceController.selectedActiveModule(moduleUI);
-        modelSpaceController.getModel().setCurrentActiveModule(modelSpaceController.getModuleList().get(moduleUI.getIdentifier()));
     }
 }
