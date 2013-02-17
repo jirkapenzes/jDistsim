fbe5ab6a47983fddb5fae64c7269eb4f371398aa
Jirka Pénzeš <jirkapenzes@hotmail.com>
2013-02-17 12:16:06 +0100

    Models register in Main frame

--- src/jDistsim/utils/pattern/mvc/AbstractFrame.java
+++ src/jDistsim/utils/pattern/mvc/AbstractFrame.java
@@ -4,6 +4,7 @@ import jDistsim.utils.ioc.IObjectContainer;
 import jDistsim.utils.ioc.ObjectContainer;
 
 import javax.swing.*;
+import java.util.Map;
 
 /**
  * Author: Jirka Pénzeš
@@ -15,19 +16,34 @@ public abstract class AbstractFrame {
     protected JFrame frame;
     protected final IObjectContainer<Class> views;
     protected final IObjectContainer<Class> controllers;
+    protected final IObjectContainer<Class> models;
     protected IComponentFactory componentFactory;
 
     public AbstractFrame(IComponentFactory componentFactory) {
         this.componentFactory = componentFactory;
         this.views = new ObjectContainer<>();
         this.controllers = new ObjectContainer<>();
+        this.models = new ObjectContainer<>();
+
+        registerAllModels();
+        initializeModels();
+
         registerAllViews();
         registerAllControllers();
     }
 
+    protected abstract void registerAllModels();
+
     protected abstract void registerAllViews();
 
     protected abstract void registerAllControllers();
+
+    private void initializeModels() {
+        for (Map.Entry<Class, Object> entry : models.entrySet()) {
+            AbstractModel model = (AbstractModel) entry.getValue();
+            model.initialize();
+        }
+    }
 
     protected abstract JFrame layout();
 
@@ -42,5 +58,9 @@ public abstract class AbstractFrame {
 
     public <Controller extends AbstractController> Controller getController(Class<Controller> controllerClass) {
         return controllers.get(controllerClass);
+    }
+
+    public <Model extends AbstractModel> Model getModel(Class<Model> modelClass) {
+        return models.get(modelClass);
     }
 }
--- src/jDistsim/utils/pattern/mvc/AbstractModel.java
+++ src/jDistsim/utils/pattern/mvc/AbstractModel.java
@@ -8,4 +8,17 @@ import jDistsim.utils.pattern.observer.Observable;
  * Time: 0:20
  */
 public abstract class AbstractModel extends Observable {
+
+    private final AbstractFrame mainFrame;
+
+    protected AbstractModel(AbstractFrame mainFrame) {
+        this.mainFrame = mainFrame;
+    }
+
+    public abstract void initialize();
+
+    protected AbstractFrame getMainFrame() {
+        return mainFrame;
+    }
+
 }
