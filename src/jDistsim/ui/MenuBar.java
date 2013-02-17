8cfb83a105928d0a373cc6dc05b0130e495c3062
Jirka Pénzeš <jirkapenzes@hotmail.com>
2013-02-17 12:39:53 +0100

    Added new menu item

--- src/jDistsim/ui/MenuBar.java
+++ src/jDistsim/ui/MenuBar.java
@@ -21,6 +21,18 @@ public class MenuBar extends JMenuBar {
         menuItem = new JMenuItem("New model");
         menu.add(menuItem);
 
+        JMenu viewMenu = new JMenu("View");
+        JCheckBoxMenuItem toolbox = new JCheckBoxMenuItem("Toolbox");
+        JCheckBoxMenuItem properties = new JCheckBoxMenuItem("Properties");
+        JCheckBoxMenuItem modules = new JCheckBoxMenuItem("Modules navigator");
+        JCheckBoxMenuItem information = new JCheckBoxMenuItem("Model information");
+
+        viewMenu.add(toolbox);
+        viewMenu.add(properties);
+        viewMenu.add(modules);
+        viewMenu.add(information);
+
         add(menu);
+        add(viewMenu);
     }
 }
