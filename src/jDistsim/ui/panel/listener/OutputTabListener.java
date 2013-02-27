5fa804fe87323431759bd44ba649d86a5d7bc902
Jirka Pénzeš <jirkapenzes@hotmail.com>
2013-02-27 01:28:51 +0100

    Added output tab listener and toolbar listener

---
+++ src/jDistsim/ui/panel/listener/OutputTabListener.java
@@ -0,0 +1,11 @@
+package jDistsim.ui.panel.listener;
+
+import jDistsim.utils.ui.IPanelListener;
+
+/**
+ * Author: Jirka Pénzeš
+ * Date: 21.2.13
+ * Time: 19:30
+ */
+public interface OutputTabListener extends IPanelListener {
+}
---
+++ src/jDistsim/ui/panel/listener/ToolbarListener.java
@@ -0,0 +1,17 @@
+package jDistsim.ui.panel.listener;
+
+import jDistsim.utils.ui.IPanelListener;
+
+import java.awt.event.MouseEvent;
+
+/**
+ * Author: Jirka Pénzeš
+ * Date: 21.2.13
+ * Time: 20:26
+ */
+public interface ToolbarListener extends IPanelListener {
+
+    void onSimulationStartButtonClick(MouseEvent mouseEvent, Object sender);
+
+    void onSimulationStopButtonClick(MouseEvent mouseEvent, Object sender);
+}
