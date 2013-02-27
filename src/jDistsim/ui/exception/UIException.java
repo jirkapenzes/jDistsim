72d7b474c8f0010786ca6104c62e5c5686e276b5
Jirka Pénzeš <jirkapenzes@hotmail.com>
2013-02-27 01:32:56 +0100

    Added base class to UI exceptions

---
+++ src/jDistsim/ui/exception/UIException.java
@@ -0,0 +1,13 @@
+package jDistsim.ui.exception;
+
+/**
+ * Author: Jirka Pénzeš
+ * Date: 26.2.13
+ * Time: 22:35
+ */
+public class UIException extends RuntimeException {
+
+    protected UIException(String message) {
+        super(message);
+    }
+}
