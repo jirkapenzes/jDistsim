f3cc50c6589f1958c991bf3219eab4236ea4bc99
Jirka Pénzeš <jirkapenzes@hotmail.com>
2013-02-27 01:26:59 +0100

    Added global dialog builder

---
+++ src/jDistsim/application/designer/common/DialogBuilder.java
@@ -0,0 +1,22 @@
+package jDistsim.application.designer.common;
+
+import javax.swing.*;
+
+/**
+ * Author: Jirka Pénzeš
+ * Date: 22.2.13
+ * Time: 0:42
+ */
+public class DialogBuilder implements IDialogBuilder {
+
+    private JFrame frame;
+
+    public DialogBuilder(JFrame frame) {
+        this.frame = frame;
+    }
+
+    @Override
+    public void buildErrorDialog(String message) {
+        JOptionPane.showMessageDialog(frame, message, "jDistSim error", JOptionPane.ERROR_MESSAGE);
+    }
+}
