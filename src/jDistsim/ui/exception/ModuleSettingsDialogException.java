98f4d66165af5a1cedf534d61463797aa2a96433
Jirka Pénzeš <jirkapenzes@hotmail.com>
2013-02-27 01:33:19 +0100

    Added UI exceptions (use in dialogs).

---
+++ src/jDistsim/ui/exception/ModuleSettingsDialogException.java
@@ -0,0 +1,13 @@
+package jDistsim.ui.exception;
+
+/**
+ * Author: Jirka Pénzeš
+ * Date: 26.2.13
+ * Time: 22:34
+ */
+public class ModuleSettingsDialogException extends UIException {
+
+    public ModuleSettingsDialogException() {
+        super("Module settings exception");
+    }
+}
---
+++ src/jDistsim/ui/exception/TypeInputException.java
@@ -0,0 +1,12 @@
+package jDistsim.ui.exception;
+
+/**
+ * Author: Jirka Pénzeš
+ * Date: 26.2.13
+ * Time: 23:44
+ */
+public class TypeInputException extends Exception {
+    public TypeInputException(String message) {
+        super(message);
+    }
+}
