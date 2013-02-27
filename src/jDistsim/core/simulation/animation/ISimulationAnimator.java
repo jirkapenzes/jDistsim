20ead12853fc97e4c23ed3d81df1c11159499bd8
Jirka Pénzeš <jirkapenzes@hotmail.com>
2013-02-27 01:38:50 +0100

    Interface for animation core

---
+++ src/jDistsim/core/simulation/animation/ISimulationAnimator.java
@@ -0,0 +1,11 @@
+package jDistsim.core.simulation.animation;
+
+/**
+ * Author: Jirka Pénzeš
+ * Date: 22.2.13
+ * Time: 22:51
+ */
+public interface ISimulationAnimator {
+
+    public void animate(String moduleFrom, String moduleTo, String iconName);
+}
