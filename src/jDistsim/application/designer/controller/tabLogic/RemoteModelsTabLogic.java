adc19a1ca3ac320338f710ccbbf37d6c17b9042e
Jirka Pénzeš <jirkapenzes@hotmail.com>
2013-03-03 00:25:22 +0100

    Remote models tab logic implemented (add, edit)

--- src/jDistsim/application/designer/controller/tabLogic/RemoteModelsTabLogic.java
+++ src/jDistsim/application/designer/controller/tabLogic/RemoteModelsTabLogic.java
@@ -1,12 +1,17 @@
 package jDistsim.application.designer.controller.tabLogic;
 
 import jDistsim.application.designer.common.Application;
+import jDistsim.application.designer.common.DialogBuilder;
 import jDistsim.application.designer.controller.InformationController;
 import jDistsim.application.designer.view.InformationView;
 import jDistsim.core.simulation.distributed.DistributedModelDefinition;
+import jDistsim.ui.dialog.BaseDialog;
 import jDistsim.ui.dialog.DistributedModelDialog;
+import jDistsim.ui.dialog.DistributedModuleSelectedDialog;
 import jDistsim.ui.panel.listener.RemoteModelsTabListener;
 import jDistsim.utils.logging.Logger;
+import jDistsim.utils.pattern.observer.IObserver;
+import jDistsim.utils.pattern.observer.Observable;
 
 import javax.swing.table.DefaultTableModel;
 import java.awt.event.MouseEvent;
@@ -18,24 +23,47 @@ import java.util.Vector;
  * Date: 2.3.13
  * Time: 17:00
  */
-public class RemoteModelsTabLogic implements RemoteModelsTabListener {
+public class RemoteModelsTabLogic implements RemoteModelsTabListener, IObserver {
 
     private InformationController controller;
+    private InformationView view;
 
     public RemoteModelsTabLogic(InformationController controller) {
         this.controller = controller;
+        view = controller.getMainFrame().getView(InformationView.class);
+
+        Application.global().getDistributedModels().addObserver(this);
         rebuildTableModel();
+        invalidateButtons();
     }
 
     @Override
     public void onOpenAddDialogButtonClick(MouseEvent mouseEvent, Object sender) {
         DistributedModelDialog dialog = new DistributedModelDialog(controller.getMainFrame().getFrame());
         dialog.showDialog();
+        if (dialog.getDialogResult() == BaseDialog.Result.OK) {
+            DistributedModelDefinition modelDefinition = dialog.getDistributedModelDefinition();
+            Application.global().getDistributedModels().put(modelDefinition.getRmiModelName(), modelDefinition);
+        }
     }
 
     @Override
     public void onOpenEditDialogButtonClick(MouseEvent mouseEvent, Object sender) {
+        DistributedModuleSelectedDialog selectDialog = new DistributedModuleSelectedDialog(
+                controller.getMainFrame().getFrame(), "Select distributed model to edit");
 
+        selectDialog.showDialog();
+        if (selectDialog.getDialogResult() == BaseDialog.Result.OK) {
+            DistributedModelDefinition modelDefinition = selectDialog.getModelDefinition();
+            if (modelDefinition == null) {
+                new DialogBuilder(controller.getMainFrame().getFrame()).buildErrorDialog("There was no model selected");
+                onOpenEditDialogButtonClick(mouseEvent, sender);
+                return;
+            } else {
+                DistributedModelDialog dialog = new DistributedModelDialog(controller.getMainFrame().getFrame(), modelDefinition);
+                dialog.showDialog();
+            }
+        }
     }
 
     @Override
@@ -76,5 +104,21 @@ public class RemoteModelsTabLogic implements RemoteModelsTabListener {
         columns.addElement("Port");
         columns.addElement("Lookahead");
         return columns;
+    }
+
+    private void invalidateButtons() {
+        if (Application.global().getDistributedModels().isEmpty()) {
+            view.getContentPane().getRemoteModelsTabPanel().getEditButton().deactivate();
+            view.getContentPane().getRemoteModelsTabPanel().getRemoveButton().deactivate();
+        } else {
+            view.getContentPane().getRemoteModelsTabPanel().getEditButton().activate();
+            view.getContentPane().getRemoteModelsTabPanel().getRemoveButton().activate();
+        }
+    }
+
+    @Override
+    public void update(Observable observable, Object arguments) {
+        rebuildTableModel();
+        invalidateButtons();
     }
 }
