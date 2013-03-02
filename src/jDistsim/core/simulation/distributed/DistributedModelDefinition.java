dd502a7bbad8b904187ef8199f64e5e8ef03b349
Jirka Pénzeš <jirkapenzes@hotmail.com>
2013-03-02 13:46:03 +0100

    Define distributed model

--- src/jDistsim/core/simulation/distributed/DistributedModelDefinition.java
+++ src/jDistsim/core/simulation/distributed/DistributedModelDefinition.java
@@ -6,4 +6,41 @@ package jDistsim.core.simulation.distributed;
  * Time: 23:14
  */
 public class DistributedModelDefinition {
+
+    private int port;
+    private String modelName;
+    private String rmiModelName;
+    private boolean lookahead;
+
+    public String getModelName() {
+        return modelName;
+    }
+
+    public void setModelName(String modelName) {
+        this.modelName = modelName;
+    }
+
+    public String getRmiModelName() {
+        return rmiModelName;
+    }
+
+    public void setRmiModelName(String rmiModelName) {
+        this.rmiModelName = rmiModelName;
+    }
+
+    public boolean isLookahead() {
+        return lookahead;
+    }
+
+    public void setLookahead(boolean lookahead) {
+        this.lookahead = lookahead;
+    }
+
+    public int getPort() {
+        return port;
+    }
+
+    public void setPort(int port) {
+        this.port = port;
+    }
 }
