c3285e6d5b16d5d9e9d93b3d74c97032a5d55b4c
Jirka Pénzeš <jirkapenzes@hotmail.com>
2013-03-02 20:07:44 +0100

    Implemented dialog for configuration distributed model dialog

--- src/jDistsim/ui/dialog/DistributedModelDialog.java
+++ src/jDistsim/ui/dialog/DistributedModelDialog.java
@@ -1,6 +1,10 @@
 package jDistsim.ui.dialog;
 
+import jDistsim.core.simulation.distributed.DistributedModelDefinition;
+import jDistsim.ui.TypeInputValidator;
+
 import javax.swing.*;
+import java.awt.*;
 
 /**
  * Author: Jirka Pénzeš
@@ -9,19 +13,93 @@ import javax.swing.*;
  */
 public class DistributedModelDialog extends BaseDialog {
 
+    private JTextField modelNameTextField;
+    private JTextField rmiModelNameTextField;
+    private JTextField addressTextField;
+    private JTextField portTextField;
+    private JCheckBox lookaheadCheckBox;
+    private DistributedModelDefinition distributedModelDefinition;
+
     public DistributedModelDialog(JFrame parent) {
-        super(parent, "Configure distributed model dialog");
+        this(parent, DistributedModelDefinition.createDefault());
     }
+
+    public DistributedModelDialog(JFrame parent, DistributedModelDefinition distributedModelDefinition) {
+        super(parent, "Adding a new distributed model");
+        this.distributedModelDefinition = distributedModelDefinition;
+    }
+
 
     @Override
     protected void initializeUI() {
+        setTitle("Configure distributed model dialog");
+        modelNameTextField.setText(distributedModelDefinition.getModelName());
+        rmiModelNameTextField.setText(distributedModelDefinition.getRmiModelName());
+        addressTextField.setText(distributedModelDefinition.getAddress());
+        portTextField.setText(String.valueOf(distributedModelDefinition.getPort()));
+        lookaheadCheckBox.setSelected(distributedModelDefinition.isLookahead());
     }
 
     @Override
     protected void buildWindowBody() {
+        modelNameTextField = getComponentFactory().makeTextField(13);
+        rmiModelNameTextField = getComponentFactory().makeTextField(13);
+        addressTextField = getComponentFactory().makeTextField();
+        portTextField = getComponentFactory().makeTextField(5);
+        lookaheadCheckBox = getComponentFactory().makeCheckBox("lookahead  (sending  null messages)");
+        setSize(new Dimension(getWidth(), 210));
+
+        constraints.gridwidth = 1;
+        build(getComponentFactory().makeLabel("RMI model name"));
+        constraints.gridwidth = GridBagConstraints.REMAINDER;
+        build(getComponentFactory().makeLabel("Model name (only local)"));
+
+        constraints.gridwidth = 1;
+        constraints.insets = new Insets(0, 0, 8, 5);
+        build(modelNameTextField);
+        constraints.gridwidth = GridBagConstraints.REMAINDER;
+        build(rmiModelNameTextField);
+
+        constraints.gridwidth = 1;
+        constraints.insets = new Insets(0, 0, 0, 0);
+        build(getComponentFactory().makeLabel("Remote address"));
+        constraints.gridwidth = GridBagConstraints.REMAINDER;
+        build(getComponentFactory().makeLabel("Remote port"));
+
+        constraints.insets = new Insets(0, 0, 8, 5);
+        constraints.gridwidth = 1;
+        build(addressTextField);
+        build(portTextField);
+        constraints.gridwidth = GridBagConstraints.REMAINDER;
+        build(getComponentFactory().makeLabel(new String()));
+
+        constraints.insets = new Insets(0, 0, 0, 0);
+        constraints.gridwidth = GridBagConstraints.REMAINDER;
+        build(lookaheadCheckBox);
     }
 
     @Override
     protected void okButtonLogic() {
+        try {
+            TypeInputValidator validator = new TypeInputValidator();
+            String modelName = validator.validateString(modelNameTextField.getText(), "Model name");
+            String rmiModelName = validator.validateString(rmiModelNameTextField.getText(), "RMI model name");
+            rmiModelName = validator.validateSpecialCharacters(rmiModelName, "RMI model name");
+            String address = validator.validateString(rmiModelNameTextField.getText(), "Remote address");
+            int port = validator.validateInteger(portTextField.getText(), "Remote port");
+            boolean lookahead = lookaheadCheckBox.isSelected();
+
+            distributedModelDefinition.setModelName(modelName);
+            distributedModelDefinition.setRmiModelName(rmiModelName);
+            distributedModelDefinition.setAddress(address);
+            distributedModelDefinition.setPort(port);
+            distributedModelDefinition.setLookahead(lookahead);
+        } catch (Exception exception) {
+            return;
+        }
+    }
+
+    public DistributedModelDefinition getDistributedModelDefinition() {
+        return distributedModelDefinition;
     }
 }
