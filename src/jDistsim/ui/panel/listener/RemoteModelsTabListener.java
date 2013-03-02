eca663582c8727d55a37f8431077ca244e0a222f
Jirka Pénzeš <jirkapenzes@hotmail.com>
2013-03-02 20:09:15 +0100

    Define remote models tab listener

--- src/jDistsim/ui/panel/listener/RemoteModelsTabListener.java
+++ src/jDistsim/ui/panel/listener/RemoteModelsTabListener.java
@@ -2,10 +2,18 @@ package jDistsim.ui.panel.listener;
 
 import jDistsim.utils.ui.IPanelListener;
 
+import java.awt.event.MouseEvent;
+
 /**
  * Author: Jirka Pénzeš
  * Date: 2.3.13
  * Time: 14:43
  */
 public interface RemoteModelsTabListener extends IPanelListener {
+
+    void onOpenAddDialogButtonClick(MouseEvent mouseEvent, Object sender);
+
+    void onOpenEditDialogButtonClick(MouseEvent mouseEvent, Object sender);
+
+    void onOpenRemoveDialogButtonClick(MouseEvent mouseEvent, Object sender);
 }
