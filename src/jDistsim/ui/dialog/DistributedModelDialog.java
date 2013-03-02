3a6d6dc5120712678bb2f61a0ed45ca03ead59f1
Jirka Pénzeš <jirkapenzes@hotmail.com>
2013-03-02 14:45:37 +0100

    Added  distributed model dialog window and listener for this window

---
+++ src/jDistsim/ui/dialog/DistributedModelDialog.java
@@ -0,0 +1,27 @@
+package jDistsim.ui.dialog;
+
+import javax.swing.*;
+
+/**
+ * Author: Jirka Pénzeš
+ * Date: 2.3.13
+ * Time: 13:53
+ */
+public class DistributedModelDialog extends BaseDialog {
+
+    public DistributedModelDialog(JFrame parent) {
+        super(parent, "Configure distributed model dialog");
+    }
+
+    @Override
+    protected void initializeUI() {
+    }
+
+    @Override
+    protected void buildWindowBody() {
+    }
+
+    @Override
+    protected void okButtonLogic() {
+    }
+}
---
+++ src/jDistsim/ui/panel/listener/RemoteModelsTabListener.java
@@ -0,0 +1,11 @@
+package jDistsim.ui.panel.listener;
+
+import jDistsim.utils.ui.IPanelListener;
+
+/**
+ * Author: Jirka Pénzeš
+ * Date: 2.3.13
+ * Time: 14:43
+ */
+public interface RemoteModelsTabListener extends IPanelListener {
+}
