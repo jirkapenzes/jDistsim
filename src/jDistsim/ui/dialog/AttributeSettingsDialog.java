f0f7242b06c2e80c3afaad1ea3c522560c6a8d67
Jirka Pénzeš <jirkapenzes@hotmail.com>
2013-03-04 09:37:13 +0100

    Dialog is now working with a concrete Attribute object

--- src/jDistsim/ui/dialog/AttributeSettingsDialog.java
+++ src/jDistsim/ui/dialog/AttributeSettingsDialog.java
@@ -1,5 +1,8 @@
 package jDistsim.ui.dialog;
 
+import jDistsim.core.simulation.simulator.entity.Attribute;
+import jDistsim.ui.TypeInputValidator;
+
 import javax.swing.*;
 import java.awt.*;
 
@@ -10,22 +13,42 @@ import java.awt.*;
  */
 public class AttributeSettingsDialog extends BaseDialog {
 
+    private Attribute attribute;
+    private JTextField nameTextField;
+    private JTextField valueTextField;
+
     public AttributeSettingsDialog(JDialog parent, String title) {
         super(parent, title);
+        this.attribute = new Attribute(new String());
     }
 
     public AttributeSettingsDialog(JFrame parent, String title) {
         super(parent, title);
+        this.attribute = new Attribute(new String());
+    }
+
+    public AttributeSettingsDialog(JDialog parent, String title, Attribute attribute) {
+        super(parent, title);
+        this.attribute = attribute;
+    }
+
+    public AttributeSettingsDialog(JFrame parent, String title, Attribute attribute) {
+        super(parent, title);
+        this.attribute = attribute;
     }
 
     @Override
     protected void initializeUI() {
-
+        nameTextField.setText(attribute.getName());
+        valueTextField.setText(attribute.getValue());
     }
 
     @Override
     protected void buildWindowBody() {
         setSize(200, 140);
+        nameTextField = getComponentFactory().makeTextField();
+        valueTextField = getComponentFactory().makeTextField();
+
         constraints.insets = new Insets(0, 0, 0, 0);
         constraints.gridwidth = 1;
         build(getComponentFactory().makeLabel("Attribute name"));
@@ -33,13 +56,27 @@ public class AttributeSettingsDialog extends BaseDialog {
         build(getComponentFactory().makeLabel("Attribute value"));
         constraints.insets = new Insets(0, 0, 8, 4);
         constraints.gridwidth = 1;
-        build(getComponentFactory().makeTextField());
+        build(nameTextField);
         constraints.gridwidth = GridBagConstraints.REMAINDER;
-        build(getComponentFactory().makeTextField());
+        build(valueTextField);
     }
 
     @Override
     protected boolean okButtonLogic() {
-        return true;
+        try {
+            TypeInputValidator validator = new TypeInputValidator();
+            String name = validator.validateString(nameTextField.getText(), "name");
+            String value = validator.validateString(valueTextField.getText(), "value");
+
+            attribute.setName(name);
+            attribute.setValue(value);
+            return true;
+        } catch (Exception exception) {
+            return false;
+        }
+    }
+
+    public Attribute getAttribute() {
+        return attribute;
     }
 }
