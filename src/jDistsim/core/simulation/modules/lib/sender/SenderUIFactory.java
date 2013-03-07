cf9367de886f0acd5bc85a2fa8830dfaed7724b7
Jirka Pénzeš <jirkapenzes@hotmail.com>
2013-03-07 12:48:33 +0100

    Use sender settings dialog

--- src/jDistsim/core/simulation/modules/lib/sender/SenderUIFactory.java
+++ src/jDistsim/core/simulation/modules/lib/sender/SenderUIFactory.java
@@ -2,6 +2,7 @@ package jDistsim.core.simulation.modules.lib.sender;
 
 import jDistsim.core.simulation.modules.IModuleUIFactory;
 import jDistsim.ui.dialog.BaseModuleSettingsDialog;
+import jDistsim.ui.dialog.SenderModuleSettingsDialog;
 
 import javax.swing.*;
 
@@ -11,8 +12,9 @@ import javax.swing.*;
  * Time: 20:49
  */
 public class SenderUIFactory implements IModuleUIFactory<Sender> {
+
     @Override
     public BaseModuleSettingsDialog makeSettingsDialog(JFrame parent, Sender module) {
-        return null;  //To change body of implemented methods use File | Settings | File Templates.
+        return new SenderModuleSettingsDialog(parent, module);
     }
 }
