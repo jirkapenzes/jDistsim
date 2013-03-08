4fe817a8679d447d990f5fe62e7d0a986123e177
Jirka Pénzeš <jirkapenzes@hotmail.com>
2013-03-08 01:52:59 +0100

    Reimplemented module properties (now from base class)

--- src/jDistsim/core/simulation/modules/lib/sender/Sender.java
+++ src/jDistsim/core/simulation/modules/lib/sender/Sender.java
@@ -1,7 +1,9 @@
 package jDistsim.core.simulation.modules.lib.sender;
 
-import jDistsim.core.simulation.distributed.DistributedModule;
+import jDistsim.core.simulation.distributed.DistributedModelDefinition;
+import jDistsim.core.simulation.distributed.DistributedSenderModule;
 import jDistsim.core.simulation.modules.ModuleConfiguration;
+import jDistsim.core.simulation.modules.common.ModuleProperty;
 import jDistsim.core.simulation.simulator.ISimulator;
 import jDistsim.core.simulation.simulator.entity.Entity;
 import jDistsim.ui.module.ModuleView;
@@ -11,7 +13,7 @@ import jDistsim.ui.module.ModuleView;
  * Date: 1.3.13
  * Time: 20:27
  */
-public class Sender extends DistributedModule {
+public class Sender extends DistributedSenderModule {
 
 
     public Sender(ModuleView view, ModuleConfiguration moduleConfiguration) {
@@ -20,6 +22,8 @@ public class Sender extends DistributedModule {
 
     @Override
     protected void initializeDefaultValues() {
+        distributedModelDefinition = DistributedModelDefinition.createNull();
+        distributedEntityKeyName = "entity";
     }
 
     @Override
@@ -32,12 +36,11 @@ public class Sender extends DistributedModule {
 
     @Override
     protected void setChildProperty() {
-        /*
-        getProperties().set(new ModuleProperty("address", modelDefinition.getAddress(), "d.address"));
-        getProperties().set(new ModuleProperty("rmiModelName", modelDefinition.getRmiModelName(), "d.rmi-name"));
-        getProperties().set(new ModuleProperty("port", modelDefinition.getPort(), "d.port"));
-        getProperties().set(new ModuleProperty("modelName", modelDefinition.getModelName(), "d.name"));
-        getProperties().set(new ModuleProperty("lookahead", modelDefinition.isLookahead(), "d.lookahead"));
-        */
+        getProperties().set(new ModuleProperty("d.address", distributedModelDefinition.getAddress(), "d.address"));
+        getProperties().set(new ModuleProperty("d.rmiModelName", distributedModelDefinition.getRmiModelName(), "d.rmi-name"));
+        getProperties().set(new ModuleProperty("d.port", distributedModelDefinition.getPort(), "d.port"));
+        getProperties().set(new ModuleProperty("d.modelName", distributedModelDefinition.getModelName(), "d.name"));
+        getProperties().set(new ModuleProperty("d.lookahead", distributedModelDefinition.isLookahead(), "d.lookahead"));
+        getProperties().set(new ModuleProperty("d.entity", distributedEntityKeyName, "d.lookahead"));
     }
 }
