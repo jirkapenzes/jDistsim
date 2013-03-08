2a76aa12761c091379f35dd0171a912583871d97
Jirka Pénzeš <jirkapenzes@hotmail.com>
2013-03-08 01:48:47 +0100

    Implemented receive module without logic (only properties)

--- src/jDistsim/core/simulation/modules/lib/receiver/Receiver.java
+++ src/jDistsim/core/simulation/modules/lib/receiver/Receiver.java
@@ -1,7 +1,8 @@
 package jDistsim.core.simulation.modules.lib.receiver;
 
-import jDistsim.core.simulation.modules.Module;
+import jDistsim.core.simulation.distributed.DistributedReceiveModule;
 import jDistsim.core.simulation.modules.ModuleConfiguration;
+import jDistsim.core.simulation.modules.common.ModuleProperty;
 import jDistsim.core.simulation.simulator.ISimulator;
 import jDistsim.core.simulation.simulator.entity.Entity;
 import jDistsim.ui.module.ModuleView;
@@ -11,14 +12,16 @@ import jDistsim.ui.module.ModuleView;
  * Date: 5.3.13
  * Time: 17:11
  */
-public class Receiver extends Module {
+public class Receiver extends DistributedReceiveModule {
 
     public Receiver(ModuleView view, ModuleConfiguration moduleConfiguration) {
         super(view, moduleConfiguration);
+        createdModule = true;
     }
 
     @Override
     protected void initializeDefaultValues() {
+        authorizedEntityName = "entity";
     }
 
     @Override
@@ -31,5 +34,6 @@ public class Receiver extends Module {
 
     @Override
     protected void setChildProperty() {
+        getProperties().set(new ModuleProperty("d.entity", authorizedEntityName, "d.entity"));
     }
 }
