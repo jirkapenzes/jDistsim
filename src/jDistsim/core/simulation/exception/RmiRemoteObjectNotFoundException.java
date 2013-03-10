c239fe8c450847434e0a500fd0bf3898e687c04e
Jirka Pénzeš <jirkapenzes@hotmail.com>
2013-03-10 12:16:10 +0100

    Exception for remote object not found

---
+++ src/jDistsim/core/simulation/exception/RmiRemoteObjectNotFoundException.java
@@ -0,0 +1,12 @@
+package jDistsim.core.simulation.exception;
+
+/**
+ * Author: Jirka Pénzeš
+ * Date: 8.3.13
+ * Time: 19:11
+ */
+public class RmiRemoteObjectNotFoundException extends BaseSimulatorException{
+    public RmiRemoteObjectNotFoundException() {
+        super("Remote object not found");
+    }
+}
