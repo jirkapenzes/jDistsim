64ac885a094b613c9ea86ddbe377acba072969b1
Jirka Pénzeš <jirkapenzes@hotmail.com>
2013-03-10 12:17:04 +0100

    Implemented sender module

--- src/jDistsim/core/simulation/modules/lib/sender/Sender.java
+++ src/jDistsim/core/simulation/modules/lib/sender/Sender.java
@@ -2,9 +2,13 @@ package jDistsim.core.simulation.modules.lib.sender;
 
 import jDistsim.core.simulation.distributed.DistributedModelDefinition;
 import jDistsim.core.simulation.distributed.DistributedSenderModule;
+import jDistsim.core.simulation.distributed.DistributedSimulator;
+import jDistsim.core.simulation.distributed.communication.IRemote;
+import jDistsim.core.simulation.exception.DistributedException;
 import jDistsim.core.simulation.modules.ModuleConfiguration;
 import jDistsim.core.simulation.modules.common.ModuleProperty;
-import jDistsim.core.simulation.simulator.ISimulator;
+import jDistsim.core.simulation.simulator.SimulatorOutput;
+import jDistsim.core.simulation.simulator.entity.Attribute;
 import jDistsim.core.simulation.simulator.entity.Entity;
 import jDistsim.ui.module.ModuleView;
 
@@ -27,11 +31,31 @@ public class Sender extends DistributedSenderModule {
     }
 
     @Override
-    protected void resetStates(ISimulator simulator) {
+    protected void resetStates(DistributedSimulator simulator) {
     }
 
     @Override
-    protected void logic(ISimulator simulator, Entity entity) {
+    protected void logic(DistributedSimulator simulator, Entity entity) {
+        try {
+            entity.getAttributes().set(new Attribute("d.entity", distributedEntityKeyName));
+            IRemote remote = simulator.getRemote(distributedModelDefinition.getRmiModelName());
+            remote.process(simulator.getLocalTime(), entity, simulator.getNetwork().getModelName());
+        } catch (Exception exception) {
+            simulator.getOutput().sendToOutput(SimulatorOutput.MessageType.Error, "Error sending");
+            throw new DistributedException();
+        }
+    }
+
+    @Override
+    public boolean isValid() {
+        boolean valid = super.isValid();
+        if (getDistributedModelDefinition().getRmiModelName().equals("null")) {
+            return false;
+        }
+        if (getDistributedModelDefinition().getPort() <= 0 || getDistributedModelDefinition().getPort() > 65535) {
+            return false;
+        }
+        return valid;
     }
 
     @Override
