5f9e936a985e22ed4a6a490ca8577d634d478f8f
Jirka Pénzeš <jirkapenzes@hotmail.com>
2013-03-10 12:13:49 +0100

    Interface to specify time-affect modules

---
+++ src/jDistsim/core/simulation/modules/ITimeAffectModule.java
@@ -0,0 +1,11 @@
+package jDistsim.core.simulation.modules;
+
+/**
+ * Author: Jirka Pénzeš
+ * Date: 8.3.13
+ * Time: 23:30
+ */
+public interface ITimeAffectModule {
+
+    double getMinimalAffectTime();
+}
