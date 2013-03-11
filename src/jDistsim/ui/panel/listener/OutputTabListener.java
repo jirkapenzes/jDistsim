813c32f10ddec29d365f31ce0a125be72d4bf80d
Jirka Pénzeš <jirkapenzes@hotmail.com>
2013-03-12 00:16:33 +0100

    Added new method to output tap listener (for simulator output panel)

--- src/jDistsim/ui/panel/listener/OutputTabListener.java
+++ src/jDistsim/ui/panel/listener/OutputTabListener.java
@@ -2,10 +2,22 @@ package jDistsim.ui.panel.listener;
 
 import jDistsim.utils.ui.IPanelListener;
 
+import java.awt.event.MouseEvent;
+
 /**
  * Author: Jirka Pénzeš
  * Date: 21.2.13
  * Time: 19:30
  */
 public interface OutputTabListener extends IPanelListener {
+
+    public void onTrashButtonClick(Object sender, MouseEvent mouseEvent);
+
+    public void onWordWrapButtonClick(Object sender, MouseEvent mouseEvent);
+
+    public void onScrollToEndButtonClick(Object sender, MouseEvent mouseEvent);
+
+    public void onCopyToClipboardButtonClick(Object sender, MouseEvent mouseEvent);
+
+    public void onSimulatorOutputDialogOpenButtonClick(Object sender, MouseEvent mouseEvent);
 }
