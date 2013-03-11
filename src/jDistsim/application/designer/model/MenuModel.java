bf52263735372a8a8f10e425a47e5c624431e85b
Jirka Pénzeš <jirkapenzes@hotmail.com>
2013-03-11 10:07:05 +0100

    Added model for menu controller

---
+++ src/jDistsim/application/designer/model/MenuModel.java
@@ -0,0 +1,20 @@
+package jDistsim.application.designer.model;
+
+import jDistsim.utils.pattern.mvc.AbstractFrame;
+import jDistsim.utils.pattern.mvc.AbstractModel;
+
+/**
+ * Author: Jirka Pénzeš
+ * Date: 10.3.13
+ * Time: 12:58
+ */
+public class MenuModel extends AbstractModel {
+
+    protected MenuModel(AbstractFrame mainFrame) {
+        super(mainFrame);
+    }
+
+    @Override
+    public void initialize() {
+    }
+}
