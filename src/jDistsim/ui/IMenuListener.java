49d63456c63c545f58f7078aa62ed86e6bf71ef4
Jirka Pénzeš <jirkapenzes@hotmail.com>
2013-03-11 09:41:29 +0100

    Added listener for menu bar

---
+++ src/jDistsim/ui/IMenuListener.java
@@ -0,0 +1,14 @@
+package jDistsim.ui;
+
+/**
+ * Author: Jirka Pénzeš
+ * Date: 10.3.13
+ * Time: 12:59
+ */
+public interface IMenuListener {
+
+    public void viewToolboxOnClick(boolean selected);
+    public void viewPropertiesClick(boolean selected);
+    public void viewModulesNavigatorOnClick(boolean selected);
+    public void viewModelInformationOnClick(boolean selected);
+}
