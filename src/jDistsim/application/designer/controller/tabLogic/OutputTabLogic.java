d6be6b2fb8cb04ab98a32375d8fe4c841f9a2124
Jirka Pénzeš <jirkapenzes@hotmail.com>
2013-03-12 00:16:49 +0100

    Prepare output tap logic method from listener

--- src/jDistsim/application/designer/controller/tabLogic/OutputTabLogic.java
+++ src/jDistsim/application/designer/controller/tabLogic/OutputTabLogic.java
@@ -2,9 +2,11 @@ package jDistsim.application.designer.controller.tabLogic;
 
 import jDistsim.application.designer.controller.InformationController;
 import jDistsim.core.simulation.simulator.Writer;
+import jDistsim.ui.dialog.SimulatorOutputDialog;
 import jDistsim.ui.panel.listener.OutputTabListener;
 
 import javax.swing.*;
+import java.awt.event.MouseEvent;
 
 /**
  * Author: Jirka Pénzeš
@@ -21,17 +23,47 @@ public class OutputTabLogic implements OutputTabListener, Writer {
 
     @Override
     public void write(String text) {
-        JTextArea textArea = controller.getModel().getOutputPanelTextArea();
+        writeToTextArea(controller.getModel().getOutputPanelTextArea(), text);
+        writeToTextArea(controller.getModel().getOutputDialogTextArea(), text);
+    }
+
+    private void writeToTextArea(JTextArea textArea, String text) {
         textArea.append(text + "\n");
 
         if (!text.contains("-----"))
             textArea.setCaretPosition(textArea.getDocument().getLength());
-
     }
 
     @Override
     public void clear() {
         controller.getModel().getOutputPanelTextArea().setText("");
+        controller.getModel().getOutputDialogTextArea().setText("");
+    }
+
+    @Override
+    public void onTrashButtonClick(Object sender, MouseEvent mouseEvent) {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public void onWordWrapButtonClick(Object sender, MouseEvent mouseEvent) {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public void onScrollToEndButtonClick(Object sender, MouseEvent mouseEvent) {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public void onCopyToClipboardButtonClick(Object sender, MouseEvent mouseEvent) {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public void onSimulatorOutputDialogOpenButtonClick(Object sender, MouseEvent mouseEvent) {
+        SimulatorOutputDialog dialog = new SimulatorOutputDialog(controller.getMainFrame().getFrame(), this, controller.getModel().getOutputDialogTextArea());
+        dialog.showDialog(false);
     }
 }
 
