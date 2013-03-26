5a1cdceb116411944b338c3e8d37e0818b3725ac
Jirka Pénzeš <jirkapenzes@hotmail.com>
2013-03-26 13:08:48 +0100

    Added dispose settings

---
+++ src/jDistsim/core/simulation/modules/lib/dispose/DisposeSettings.java
@@ -0,0 +1,15 @@
+package jDistsim.core.simulation.modules.lib.dispose;
+
+import jDistsim.core.simulation.modules.ModuleSettings;
+
+/**
+ * Author: Jirka Pénzeš
+ * Date: 24.3.13
+ * Time: 12:55
+ */
+public class DisposeSettings extends ModuleSettings {
+
+    public DisposeSettings(String baseIdentifier) {
+        super(baseIdentifier);
+    }
+}
