cda105eb45a2629babbc14cbfef5aff33a1d2ec6
Jirka Pénzeš <jirkapenzes@hotmail.com>
2013-04-01 12:27:29 +0200

    Added model path to application config

--- src/jDistsim/application/designer/common/Application.java
+++ src/jDistsim/application/designer/common/Application.java
@@ -2,18 +2,20 @@ package jDistsim.application.designer.common;
 
 import jDistsim.core.simulation.distributed.DistributedModelDefinition;
 import jDistsim.utils.collection.observable.ObservableHashMap;
+import jDistsim.utils.pattern.observer.Observable;
 
 /**
  * Author: Jirka Pénzeš
  * Date: 10.2.13
  * Time: 15:23
  */
-public class Application {
+public class Application extends Observable {
 
     private static Application instance;
     private static Object lock = new Object();
 
     private String modelName = "model1.jdsim";
+    private String modelFullPath = "";
     private ObservableHashMap<String, DistributedModelDefinition> distributedModels;
     private LocalNetworkSettings networkSettings;
 
@@ -39,6 +41,16 @@ public class Application {
 
     public void setModelName(String modelName) {
         this.modelName = modelName;
+        setChanged();
+    }
+
+    public String getModelFullPath() {
+        return modelFullPath;
+    }
+
+    public void setModelFullPath(String modelFullPath) {
+        this.modelFullPath = modelFullPath;
+        setChanged();
     }
 
     public ObservableHashMap<String, DistributedModelDefinition> getDistributedModels() {
