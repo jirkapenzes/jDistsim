eb11ae4a56bd8c22892b0f7fe8e4cc487092e347
jirkapenzes <jirkapenzes@gmail.com>
2013-05-18 15:23:34 +0200

    Added method to catch interrupted exception

--- src/jDistsim/utils/common/ThreadWaiter.java
+++ src/jDistsim/utils/common/ThreadWaiter.java
@@ -10,8 +10,16 @@ public class ThreadWaiter {
     public static void waitCurrentThreadFor(long millis) {
         try {
             Thread.sleep(millis);
-        } catch (InterruptedException e) {
+        } catch (InterruptedException exception) {
         }
     }
 
+    public static void waitCurrentThreadFor(long millis, boolean throwsException) throws InterruptedException {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException exception) {
+            if (throwsException)
+                throw exception;
+        }
+    }
 }
